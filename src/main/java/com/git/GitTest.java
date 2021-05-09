6e9b739e3dda7bce46b6e10556085cf1be15c3d4
zhouli <zhouli@163.com>
2021-05-09 10:31:37 +0800

    master commit1

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hell git2");
         System.out.println("hell git3");
         System.out.println("hell git4");
+        System.out.println("master test");
     }
 }
