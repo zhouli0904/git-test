37c962fcad8df8b18af58a1c07cc3d4ae46d394e
zhouli <zhouli@163.com>
2021-05-09 10:33:50 +0800

    hot-fix 修改错误

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -2,10 +2,10 @@ package com.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hell git");
-        System.out.println("hell git2");
-        System.out.println("hell git3");
-        System.out.println("hell git4");
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
         System.out.println("hot-fix test");
     }
 }
