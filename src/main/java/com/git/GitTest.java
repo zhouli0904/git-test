4430e5fb393206cb501635d3efed791b61867a04
zhouli <zhouli@163.com>
2021-05-09 10:09:10 +0800

    third commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hell git");
         System.out.println("hell git2");
+        System.out.println("hell git3");
     }
 }
